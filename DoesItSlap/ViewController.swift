82cd9a77e2a57984749bb0fef9820928be8868a6
mitVCU <mit.na14@gmail.com>
2022-01-04 18:36:20 -0500

    Initial Commit

---
+++ DoesItSlap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoesItSlap
+//
+//  Created by Mit Amin on 1/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DoesItSlapUITests/DoesItSlapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DoesItSlapUITestsLaunchTests.swift
+//  DoesItSlapUITests
+//
+//  Created by Mit Amin on 1/4/22.
+//
+
+import XCTest
+
+class DoesItSlapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
